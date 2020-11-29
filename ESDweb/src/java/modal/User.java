7137e43017daed3850688d0ac499b216e7a93efe
eligrealish <eli@grealish.org>
2020-11-29 22:51:49 +0000

    Update User.java

---
+++ ESDweb/src/java/modal/User.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modal;
+
+/**
+ *
+ * @author Eli
+ */
+public class User {
+    private String userName;
+    private String userPass;
+    private String userRole;    //Admin, Patient, Staff
+    private String userAddress;
+    private boolean userValid;
+    
+    public User() {
+    }
+
+    public User(String userName, String userPass) {
+        this.userName = userName;
+        this.userPass = userPass;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public boolean isUserValid() {
+        return userValid;
+    }
+
+    public void setUserValid(boolean userValid) {
+        this.userValid = userValid;
+    }
+    
+}
